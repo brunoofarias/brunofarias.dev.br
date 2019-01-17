90ac3920448090700510874e8c2ceefa55b360a0
BrunoFFari <bruno27ffarias@gmail.com>
2019-01-17 09:10:42 -0200

    add build project

---
+++ build/precache-manifest.62679d43da410eaab30c631223338458.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "72e3facf0fbcedbd548f",
+    "url": "/static/js/main.72e3facf.chunk.js"
+  },
+  {
+    "revision": "5edc4c43bc563b8ad4e3",
+    "url": "/static/js/1.5edc4c43.chunk.js"
+  },
+  {
+    "revision": "72e3facf0fbcedbd548f",
+    "url": "/static/css/main.0a46750f.chunk.css"
+  },
+  {
+    "revision": "5edc4c43bc563b8ad4e3",
+    "url": "/static/css/1.f40d17d9.chunk.css"
+  },
+  {
+    "revision": "51047194d4f410382e741786d3d7a07a",
+    "url": "/index.html"
+  }
+];
