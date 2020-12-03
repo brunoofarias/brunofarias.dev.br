91342263deb9eecff89c230c40ed850229f38cba
brunoofarias <bruno27ffarias@gmail.com>
2020-12-02 21:03:10 -0300

    publish new version

---
+++ docs/precache-manifest.2a5280fc94d05d09b734d43e32e3a3d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9c3588477e240b555e5fda23e567063",
+    "url": "index.html"
+  },
+  {
+    "revision": "f8cfeb8dff35702e6431",
+    "url": "static/css/2.9bf2d92f.chunk.css"
+  },
+  {
+    "revision": "3a25d33aef2110cb4a12",
+    "url": "static/css/main.b9867180.chunk.css"
+  },
+  {
+    "revision": "f8cfeb8dff35702e6431",
+    "url": "static/js/2.d642c6e6.chunk.js"
+  },
+  {
+    "revision": "4965e7f8ea7923f17aa5edfd5440b196",
+    "url": "static/js/2.d642c6e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a25d33aef2110cb4a12",
+    "url": "static/js/main.b73f9567.chunk.js"
+  },
+  {
+    "revision": "8f2579ff55bd5aa1997a",
+    "url": "static/js/runtime-main.87831706.js"
+  }
+]);
